687730d36fd21d869a3d36d0660dc0f45173d27e
MasterHiei <ruhrg.lcx@gmail.com>
2019-01-30 15:37:42 +0900

    update layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>
-    <article-list
-      :articles="articles"
-      :count="count"
-    />
-  </div>
+  <v-container
+    fluid
+    grid-list-sm
+  >
+    <v-layout
+      row
+    >
+      <v-flex
+        d-flex
+        xs9
+      >
+        <v-layout
+          column
+        >
+          <v-flex
+            v-for="(article, index) in articles"
+            :id="article._id"
+            :key="index"
+            d-flex
+          >
+            <article-list
+              :article="article"
+            />
+          </v-flex>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        d-flex
+        xs3
+      >
+        right-side-area
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -22,7 +51,7 @@ export default {
     ...mapGetters({
       articles: 'articles/all',
       count: 'articles/count',
-    })
+    }),
   },
 }
 </script>
