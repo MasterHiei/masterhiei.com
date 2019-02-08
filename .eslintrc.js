65b939e17c525c9bbd9d5aec1c49cb0fada130a1
MasterHiei <ruhrg.lcx@gmail.com>
2019-02-08 15:43:08 +0900

    edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,23 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     'eslint:recommended',
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   // required to lint *.vue files
-  plugins: ['vue'],
+  plugins: [
+    'vue',
+    'prettier',
+  ],
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -23,6 +26,12 @@ module.exports = {
     semi: ['error', 'always'],
     'vue/max-attributes-per-line': 'off',
     'comma-dangle': ['error', 'always-multiline'],
-    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5', semi: true }]
-  }
-}
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true,
+        trailingComma: 'es5',
+        semi: true,
+      },
+    ],
+  },
+};
