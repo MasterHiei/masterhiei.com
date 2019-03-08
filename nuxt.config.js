ead27a7a75b3228e88b6834dc4bcc299c7075681
MasterHiei <ruhrg.lcx@gmail.com>
2019-03-08 11:02:31 +0900

    adjust package dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       {
         rel: 'stylesheet',
-        href: 'https://use.fontawesome.com/releases/v5.7.1/css/all.css',
+        href: 'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
         integers:
           'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr',
         crossOrigin: 'anonymous',
