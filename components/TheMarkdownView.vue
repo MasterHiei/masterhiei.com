f8e78a1abfc4d70310102d805caceed5ff015f48
MasterHiei <ruhrg.lcx@gmail.com>
2019-04-01 16:57:03 +0900

    show markdown content by mavon-editor

--- components/TheMarkdownView.vue
+++ components/TheMarkdownView.vue
@@ -1,10 +1,29 @@
 <template>
-  <!-- eslint-disable-next-line vue/no-v-html -->
-  <v-flex wrap v-html="$md.render(content)" />
+  <v-flex wrap>
+    <no-ssr>
+      <mavon-editor
+        :value="content"
+        class="mavon-preview justify-center"
+        :tab-size="Number(2)"
+        :box-shadow="false"
+        :toolbars-flag="false"
+        :subfield="false"
+        default-open="preview"
+      />
+    </no-ssr>
+  </v-flex>
 </template>
 
 <script>
+import { mavonEditor } from 'mavon-editor';
+import 'mavon-editor/dist/css/index.css';
+mavonEditor.getMarkdownIt();
+
 export default {
+  components: {
+    mavonEditor,
+  },
+
   props: {
     content: {
       type: String,
@@ -15,6 +34,16 @@ export default {
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
->>>code:before, >>>code:after
-  content none
+.mavon-preview
+  height 100%
+  width 100%
+  min-height 0
+  min-width 0
+  z-index 1
+
+>>>.v-note-panel
+  border-style none !important
+
+>>>.v-show-content
+  padding 0 !important
 </style>
