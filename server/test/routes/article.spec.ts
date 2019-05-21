71c5782d343b73aeb0f621ac8b6317a22ff628f7
MasterHiei <ruhrg.lcx@gmail.com>
2019-05-21 17:46:14 +0900

    feat: complement test cases

--- server/test/routes/article.spec.ts
+++ server/test/routes/article.spec.ts
@@ -1,9 +1,10 @@
 import { Server } from 'http';
 import request, { SuperTest, Test } from 'supertest';
+import mongoose from 'mongoose';
 import app from '../../app';
 
-// Base path
-const path = '/api/v1/articles';
+// Base url
+const url = '/api/v1/articles';
 
 // Instance of test server and request
 let server!: Server, agent: SuperTest<Test>;
@@ -24,19 +25,78 @@ afterAll(
   }
 );
 
-// Routers tests
-describe('Article Routing Test', (): void => {
-  // Success
+// Routing tests
+describe('Testing Article Routing', (): void => {
+  // Get articles with query params
   describe('Get articles with query params', (): void => {
-    it('Should be return 200', (): void => {
-      agent.get(`${path}?page=1&limit=4`).expect(200);
+    // Successful request
+    describe('Success', (): void => {
+      it('Return status 200', async (): Promise<void> => {
+        const response = await agent.get(url).query({ page: 1, limit: 4 });
+        expect(response.status).toBe(200);
+      });
+    });
+
+    // Missing all params
+    describe('Failure: Missing all params', (): void => {
+      it('Return status 422', async (): Promise<void> => {
+        const response = await agent.get(url);
+        expect(response.status).toBe(422);
+      });
+    });
+
+    // Missing page
+    describe('Failure: Missing page', (): void => {
+      it('Return status 422', async (): Promise<void> => {
+        const response = await agent.get(url).query({ limit: 4 });
+        expect(response.status).toBe(422);
+      });
+    });
+
+    // Missing limit
+    describe('Failure: Missing limit', (): void => {
+      it('Return status 422', async (): Promise<void> => {
+        const response = await agent.get(url).query({ page: 1 });
+        expect(response.status).toBe(422);
+      });
+    });
+
+    // Incorrect page type
+    describe('Failure: Incorrect page type', (): void => {
+      it('Return status 422', async (): Promise<void> => {
+        const response = await agent.get(url).query({ page: 'a' });
+        expect(response.status).toBe(422);
+      });
+    });
+
+    // Incorrect limit type
+    describe('Failure: Incorrect limit type', (): void => {
+      it('Return status 422', async (): Promise<void> => {
+        const response = await agent.get(url).query({ limit: 'b' });
+        expect(response.status).toBe(422);
+      });
     });
   });
 
-  // Failure
-  describe('Get articles without query params', (): void => {
-    it('Should be return 422', (): void => {
-      agent.get(path).expect(422);
+  // Get an article with id
+  describe('Get an article with id', (): void => {
+    // TODO: Successful request
+
+    // Incorrect id type
+    describe('Failure: Incorrect id type', (): void => {
+      it('Return status 422', async (): Promise<void> => {
+        const response = await agent.get(`${url}/id`);
+        expect(response.status).toBe(422);
+      });
+    });
+
+    // No result
+    describe('Failure: No result', (): void => {
+      it('Return status 404', async (): Promise<void> => {
+        const id = mongoose.Types.ObjectId();
+        const response = await agent.get(`${url}/${id}`);
+        expect(response.status).toBe(404);
+      });
     });
   });
 });
